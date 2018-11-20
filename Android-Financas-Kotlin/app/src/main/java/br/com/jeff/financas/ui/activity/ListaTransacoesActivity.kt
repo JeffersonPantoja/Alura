3f52616b5e6022388c0dbb656a4e63f53a3ec7ad
Jefferson Pantoja <jeffkd35@gmail.com>
2018-11-20 16:56:59 -0300

    aula 04

--- Android-Financas-Kotlin/app/src/main/java/br/com/jeff/financas/ui/activity/ListaTransacoesActivity.kt
+++ Android-Financas-Kotlin/app/src/main/java/br/com/jeff/financas/ui/activity/ListaTransacoesActivity.kt
@@ -1,13 +1,21 @@
 package br.com.jeff.financas.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import br.com.jeff.financas.Extension.formataParaBrasileiro
 import br.com.jeff.financas.R
 import br.com.jeff.financas.model.Tipo
 import br.com.jeff.financas.model.Transacao
 import br.com.jeff.financas.ui.ResumoView
 import br.com.jeff.financas.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
 import java.util.*
 
@@ -19,8 +27,45 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         val transacoes: List<Transacao> = listaDeExemplo()
         configuraAdapter(transacoes)
+        configuraResumo(transacoes)
 
+        lista_transacoes_adiciona_receita.setOnClickListener {
 
+            val view : View = window.decorView
+            val viewCriada = LayoutInflater.from(this).inflate(R.layout.form_transacao, view as ViewGroup, false)
+
+            val hoje = Calendar.getInstance()
+            viewCriada.form_transacao_data.setText(hoje.formataParaBrasileiro())
+
+            val ano = 2018
+            val mes = 11
+            val dia = 20
+
+            viewCriada.form_transacao_data.setOnClickListener {
+                DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                    val dataSelecionada = Calendar.getInstance()
+                    dataSelecionada.set(ano, mes, dia)
+                    viewCriada.form_transacao_data.setText(dataSelecionada.formataParaBrasileiro())
+                }, ano, mes, dia)
+                    .show()
+            }
+
+            val adapter = ArrayAdapter.createFromResource(this,
+                R.array.categorias_de_receita,
+                android.R.layout.simple_spinner_dropdown_item)
+
+            viewCriada.form_transacao_categoria.adapter = adapter
+
+            AlertDialog.Builder(this)
+                .setTitle("Alert dialog")
+                .setView(viewCriada)
+                .setPositiveButton("Adicionar",null)
+                .setNegativeButton("Cancelar",null)
+                .show()
+        }
+    }
+
+    private fun configuraResumo(transacoes: List<Transacao>) {
         val resumoView = ResumoView(this, window.decorView, transacoes)
         resumoView.atualiza()
     }
